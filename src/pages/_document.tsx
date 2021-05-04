c66f1b30a9cbf3d041655ae2c5d5993d73016a32
Ilham Pratama <ilham.pratama@wahyoo.com>
2021-05-04 22:12:44 +0700

    update next version to latest and update linter and remove prettier eslint

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,7 @@
 import React from 'react';
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext
-} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render(): React.ReactElement {
     return (
       <Html>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-export default function Home(): React.ReactElement {
+const Home: React.FC = () => {
   return (
     <div>
-      <p className="text-center px-20 font-bold">
+      <p className="text-center text-2xl px-20 font-bold">
         Next.js + TypeScript + Tailwind = Production Ready App
       </p>
     </div>
   );
-}
+};
+
+export default Home;
